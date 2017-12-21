e63172bed196536c80d7bd82af848ff94815493b
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2017-12-21 17:31:24 +0900

    Several changes to make CV work
    Refs #1102

--- almasparsemodeling/core/cv.py
+++ almasparsemodeling/core/cv.py
@@ -5,6 +5,7 @@ import scipy
 import itertools
 
 import almasparsemodeling.external.sakura as sakura
+from . import gridder
 from . import util
 
 
@@ -14,7 +15,7 @@ class GriddedVisibilitySubsetHandler(object):
         self.uvgrid = uvgridconfig
         self.num_fold = num_fold
         
-        self._clear
+        self._clear()
         
     def _clear(self):
         self.visibility_active = None
@@ -29,6 +30,7 @@ class GriddedVisibilitySubsetHandler(object):
         # amplitude should be nonzero in active pixels
         self.active_index = numpy.where(numpy.logical_and(grid_real != 0, grid_imag != 0))
         num_active = len(self.active_index[0])
+        print 'num_active={0}'.format(num_active)
     
         # random index 
         self.index_generator = util.VisibilitySubsetGenerator(num_active, self.num_fold)
@@ -39,6 +41,7 @@ class GriddedVisibilitySubsetHandler(object):
         # grid data shape: (nv, nu, npol, nchan)
         grid_real = self.visibility.real
         grid_imag = self.visibility.imag
+        wgrid_real = self.visibility.wreal
         gdata_shape = grid_real.shape
         
         # random index 
@@ -55,7 +58,8 @@ class GriddedVisibilitySubsetHandler(object):
         # | 6| 7| 8|
         # | 3| 4| 5|
         # | 0| 1| 2|
-        u = sakura.empty_aligned((num_subvis), dtype=numpy.float64)
+        num_subvis = len(random_index)
+        u = sakura.empty_aligned((num_subvis,), dtype=numpy.float64)
         v = sakura.empty_like_aligned(u)
         uid = self.active_index[1][random_index]
         vid = self.active_index[0][random_index]
@@ -67,47 +71,55 @@ class GriddedVisibilitySubsetHandler(object):
         nv = self.uvgrid.nv
         assert gdata_shape[0] == nv
         assert gdata_shape[1] == nu
-        u[:] = (uid % nu - offsetu) * cellu
-        v[:] = (vid / nu - offsetv) * cellv
+        #print 'subset ID {0}: uid{0}={1}; vid{0}={2}'.format(self.subset_id, uid.tolist(), vid.tolist())
+        u[:] = (uid - offsetu) * cellu
+        v[:] = (vid - offsetv) * cellv
         
         # visibility data to be cached
         # here, we assume npol == 1 (Stokes visibility I_v) and nchan == 1
         assert len(gdata_shape) == 4
         assert gdata_shape[2] == 1 # npol should be 1
         assert gdata_shape[3] == 1 # nchan should be 1 
-        data_shape = (num_subvis)
-        real = sakura.empty_like_aligned(data_shape, dtype=numpy.float32)
+        real = sakura.empty_aligned((num_subvis,), dtype=numpy.float32)
         imag = sakura.empty_like_aligned(real)
+        wreal = sakura.empty_like_aligned(real)
         
         real[:] = grid_real[self.active_index][random_index]
         imag[:] = grid_imag[self.active_index][random_index]
+        wreal[:] = wgrid_real[self.active_index][random_index]
         
         # generate subset
         self.visibility_active = self.visibility
         self.__replace_with(self.visibility_active.real, random_index, 0.0)
         self.__replace_with(self.visibility_active.imag, random_index, 0.0)
-        self.visibility_cache = [gridder.GridderWorkingSet(rdata=real, 
+        self.__replace_with(self.visibility_active.wreal, random_index, 0.0)
+        self.visibility_cache = [gridder.GridderWorkingSet(data_id=0, # nominal data ID
+                                                           rdata=real, 
                                                            idata=imag,
+                                                           weight=wreal,
                                                            u=u,
                                                            v=v)]
         
     def restore_visibility(self):
         if self.visibility_active is not None and self.visibility_cache is not None:
-            self.__replace_with(self.visibility.real, self.visibility_cache.rdata)
-            self.__replace_with(self.visibility.imag, self.visibility_cache.idata)
+            random_index = self.index_generator.get_subset_index(self.subset_id)
+            for cache in self.visibility_cache:
+                self.__replace_with(self.visibility.real, random_index, cache.rdata)
+                self.__replace_with(self.visibility.imag, random_index, cache.idata)
+                self.__replace_with(self.visibility.wreal, random_index, cache.weight)
             self._clear()
             
     def __replace_with(self, src, index_list, newval):
-        src[self.active_index][index_list] = newval
+        replace_index = tuple(map(lambda x: x[index_list], self.active_index))
+        src[replace_index] = newval
 
 class MeanSquareErrorEvaluator(object):
     def __init__(self):
         self.mse_storage = numpy.empty(100, dtype=numpy.float64)
         self.num_mse = 0
         
-    def _evaluate_mse(self, visibility_cache, image, imageparam):
+    def _evaluate_mse(self, visibility_cache, image, uvgrid):
         # UV grid configuration parameters
-        uvgrid = imageparam.uvgridconfig
         offsetu = uvgrid.offsetu
         offsetv = uvgrid.offsetv
         nu = uvgrid.nu
@@ -117,16 +129,18 @@ class MeanSquareErrorEvaluator(object):
         
         # Obtain visibility from image array
         shifted_image = numpy.fft.fftshift(image)
-        shifted_imagefft = numpy.fft2(shifted_image, norm='ortho')
+        shifted_imagefft = numpy.fft.fft2(shifted_image, norm='ortho')
         imagefft = numpy.fft.fftshift(shifted_imagefft)
         rmodel = numpy.flipud(imagefft.real.transpose())
         imodel = numpy.flipud(imagefft.imag.transpose())
         
         # Compute MSE
         mse = 0.0
-        num_mse = 0
+        num_terms = 0
         rinterp = scipy.interpolate.interp2d(numpy.arange(nv), numpy.arange(nu), rmodel)
         iinterp = scipy.interpolate.interp2d(numpy.arange(nv), numpy.arange(nu), imodel)
+        uid = []
+        vid = []
         for ws in visibility_cache:
             u = ws.u
             v = ws.v
@@ -134,28 +148,30 @@ class MeanSquareErrorEvaluator(object):
             idata = ws.idata
             pu = u / cellu + offsetu
             pv = v / cellv + offsetv
+            uid.append(pu)
+            vid.append(pv)
             for p, q, x, y in itertools.izip(pu, pv, rdata, idata):
                 a = rinterp(q, p)
                 b = iinterp(q, p)
                 dx = x - a
                 dy = y - b
                 mse += dx * dx + dy * dy
-                num_mse += 1
-        mse /= num_mse
+                num_terms += 1
+        mse /= num_terms
         return mse
             
         
-    def evaluate_and_accumulate(self, visibility_cache, image, imageparam):
+    def evaluate_and_accumulate(self, visibility_cache, image, uvgridconfig):
         """
         Evaluate MSE (Mean Square Error) from image which is a solution of MFISTA
         and visibility_cache provided as a set of GridderWorkingSet instance.
         """
         # TODO: evaluate MSE
-        mse = self._evaluate_mse(visibility_cache, image, imageparam)
+        mse = self._evaluate_mse(visibility_cache, image, uvgridconfig)
         
         # register it
-        if self.num_mse <= len(self.mse_storage):
-            self.mse_storage = self.mse_storage.resize(len(self.mse_storage) + 100)
+        if self.num_mse >= len(self.mse_storage):
+            self.mse_storage = numpy.resize(self.mse_storage, len(self.mse_storage) + 100)
         self.mse_storage[self.num_mse] = mse
         self.num_mse += 1
         
