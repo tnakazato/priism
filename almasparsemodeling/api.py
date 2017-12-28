70a554a0d55faf24b961e217d2684fc5351c4710
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2017-12-28 16:35:20 +0900

    Define CV optimization method to api.py
    Refs #1102

--- almasparsemodeling/api.py
+++ almasparsemodeling/api.py
@@ -1,9 +1,17 @@
 from __future__ import absolute_import
 
+import os
+import shutil
+import math
 import numpy
+import collections
+import pylab as pl
+import matplotlib
+import time
 
 import almasparsemodeling.external.sakura as sakura
 import almasparsemodeling.core as core
+import almasparsemodeling.external.casa as casa
 
 class AlmaSparseModelingResult(object):
     """
@@ -226,7 +234,181 @@ class AlmaSparseModeling(AlmaSparseModelingCore):
         imagemeta = core.ImageMetaInfoContainer.fromvis(vis)
         writer = core.ImageWriter(self.imparam, self.imagearray, imagemeta)
         writer.write(overwrite=overwrite)
+        
+    def cvforgridvis(self, l1_list, ltsv_list, imageprefix, imagepolicy='full', 
+               summarize=True, figfile=None, datafile=None):
+        """
+        Perform cross validation and search the best parameter for L1 and Ltsv from 
+        the given list of these.
+        
+        Inputs:
+            l1_list -- list of L1 values to examine
+            ltsv_list -- List of Ltsv values to examine
+            imageprefix -- prefix for output image
+                           imageprefix is used for the best image (<imageprefix>.fits) 
+            imagepolicy -- policy of output image ('full' or 'best')
+                           full: keep all FITS image produced by cross validation
+                           best: only keep FITS image corersponding to the best solution
+            summarize -- generate summary plot if True
+            figfile -- name of summary figure of cross validation. 
+                       None will not produce a file.
+            datafile -- name of output data file containing whole MSE values. 
+                        None will not produce a file.
+        
+        Output:
+            dictionary containing best L1 (key: L1), best Ltsv (key;Ltsv), and
+            corresponding image name (key: image, should be <imageprefix>.fits)
+        """
+        start_time = time.time()
+        
+        # sanity check
+        if imagepolicy not in ('best', 'full'):
+            raise ArgumentError('imagepolicy must be best or full. {0} was provided.'.format(imagepolicy))
+        
+        try:
+            np_l1_list = numpy.asarray(l1_list)
+            np_ltsv_list = numpy.asarray(ltsv_list)
+        except Exception, e:
+            print 'Exception occurred'
+            print str(e)
+            raise ArgumentError('l1_list or ltsv_list (or both) seems invalid.')
+        
+        if str(np_l1_list.dtype) == 'object':
+            raise ArgumentError('l1_list contains invalid value')
+        if str(np_ltsv_list.dtype) == 'object':
+            raise ArgumentError('ltsv_list contains invalid value')
+        
+        result_L1 = []
+        result_Ltsv = []
+        result_mse = []
+        result_image = []
+        
+        num_L1 = len(np_l1_list)
+        num_Ltsv = len(np_ltsv_list)
+        
+        if summarize is True:
+            PlotterClass = CVPlotter
+        else:
+            PlotterClass = NullPlotter
+        plotter = PlotterClass(num_L1, num_Ltsv, np_l1_list, np_ltsv_list)
+            
+        if datafile is not None:
+            f = open('cvresult.dat', 'w')
+        else:
+            f = open(os.devnull, 'w')
+        print >> f, '# L1, Ltsv, MSE'
+        
+        # initialize CV
+        self.initializecv(num_fold=10)
+                
+        for i, L1 in enumerate(np_l1_list):
+            for j, Ltsv in enumerate(np_ltsv_list):
+                result_L1.append(L1)
+                result_Ltsv.append(Ltsv)
+                mse = self.computegridmse(L1, Ltsv)
+                result_mse.append(mse)
+                imagename = 'L1_{0}_Ltsv_{1}.fits'.format(int(math.log10(L1)), int(math.log10(Ltsv)))
+                self.mfista(L1, Ltsv)
+                self.exportimage(imagename, overwrite=True)
+                result_image.append(imagename)
+                print 'L1 10^{0} Ltsv 10^{1}: MSE {2} FITS {3}'.format(int(math.log10(L1)),
+                                                                       int(math.log10(Ltsv)),
+                                                                       mse,
+                                                                       imagename)
+                print >> f, '{0}, {1}, {2}'.format(L1, Ltsv, mse)
+        
+                if summarize is True:
+                    with casa.OpenImage(imagename) as ia:
+                        chunk = ia.getchunk()
+                    data = numpy.squeeze(chunk) # data will be 2D
+                    
+                    plotter.plotimage(i, j, data, mse)
+                            
+        # finalize CV
+        self.finalizecv()
+        
+        f.close()
+        
+        best_solution = numpy.argmin(result_mse)
+        best_mse = result_mse[best_solution]
+        best_image = result_image[best_solution]
+        best_L1 = result_L1[best_solution]
+        best_Ltsv = result_Ltsv[best_solution]
+        
+        L1_index = np_l1_list.tolist().index(best_L1)
+        Ltsv_index = np_ltsv_list.tolist().index(best_Ltsv)
+        plotter.mark_bestimage(L1_index, Ltsv_index)
+        
+        plotter.draw()
+        if figfile is not None:
+            plotter.savefig(figfile)
+        # completed
+        end_time = time.time()
+        
+        print 'Process completed. Optimal result is as follows'
+        print '    L1, Ltsv = 10^{0}, 10^{1}'.format(int(math.log10(best_L1)), int(math.log10(best_Ltsv)))
+        print '    MSE = {0}'.format(best_mse)
+        print '    imagename = {0}'.format(best_image)
+        
+        print 'Elapsed {0} sec'.format(end_time - start_time)
+        
+        
+        # copy the best image to final image
+        shutil.copy2(best_image, imageprefix+'.fits')
+        if imagepolicy == 'full':
+            # keep all intermediate images
+            pass
+        elif imagepolicy == 'best':
+            # remove all intermediate images
+            for imagename in result_image:
+                os.remove(imagename)
+        else:
+            assert False
+            
+        # finally, return best L1 and Ltsv
+        return {'L1': best_L1, 'Ltsv': best_Ltsv}
+           
 
+    def initializecv(self, num_fold=10):
+        assert self.griddedvis is not None
+        
+        if (not hasattr(self, 'visset')) or self.visset is None:
+            self.visset = core.VisibilitySubsetGenerator(self.griddedvis, num_fold) 
+    
+    def finalizecv(self):
+        self.visset = None
+    
+    def computegridmse(self, l1, ltsv):
+        """
+        Compute mean-square-error (MSE) on resulting image.
+        MSE is evaluated from gridded visibility.
+        """
+        mfistaparam = core.ParamContainer.CreateContainer(core.MfistaParamContainer, 
+                                                          l1=l1, ltsv=ltsv)
+        assert self.griddedvis is not None
+        
+        evaluator = core.MeanSquareErrorEvaluator()
+        num_fold = self.visset.num_fold
+        subset_handler = core.GriddedVisibilitySubsetHandler(self.visset, 
+                                                             self.uvgridconfig)
+        
+        for i in xrange(num_fold):
+            # pick up subset for cross validation
+            with subset_handler.generate_subset(subset_id=i) as subset:
+            
+                # run MFISTA
+                imagearray = self._mfista(mfistaparam, 
+                                          subset.visibility_active)
+
+                # evaluate MSE (Mean Square Error)
+                mse = evaluator.evaluate_and_accumulate(subset.visibility_cache, 
+                                                        imagearray,
+                                                        self.uvgridconfig)
+             
+        mean_mse = evaluator.get_mean_mse()
+        
+        return mean_mse
+    
     def computemse(self, num_fold=10):
         """
         Compute mean-square-error (MSE) on resulting image.
@@ -264,50 +446,6 @@ class AlmaSparseModeling(AlmaSparseModelingCore):
         
         return mean_mse
     
-    def initializecv(self, num_fold=10):
-        assert self.griddedvis is not None
-        
-        if (not hasattr(self, 'visset')) or self.visset is None:
-            self.visset = core.VisibilitySubsetGenerator(self.griddedvis, num_fold) 
-    
-    def finalizecv(self):
-        self.visset = None
-    
-    def computegridmse(self, l1, ltsv):
-        """
-        Compute mean-square-error (MSE) on resulting image.
-        MSE is evaluated from gridded visibility.
-        """
-        mfistaparam = core.ParamContainer.CreateContainer(core.MfistaParamContainer, 
-                                                          l1=l1, ltsv=ltsv)
-        assert self.griddedvis is not None
-        #real_data = self.griddedvis.real
-        #active = numpy.flatnonzero(real_data)
-        #num_active = len(active)
-        
-        evaluator = core.MeanSquareErrorEvaluator()
-        #visset = core.VisibilitySubsetGenerator(self.griddedvis, num_fold)
-        num_fold = self.visset.num_fold
-        subset_handler = core.GriddedVisibilitySubsetHandler(self.visset, 
-                                                             self.uvgridconfig)
-        
-        for i in xrange(num_fold):
-            # pick up subset for cross validation
-            with subset_handler.generate_subset(subset_id=i) as subset:
-            
-                # run MFISTA
-                imagearray = self._mfista(mfistaparam, 
-                                          subset.visibility_active)
-
-                # evaluate MSE (Mean Square Error)
-                mse = evaluator.evaluate_and_accumulate(subset.visibility_cache, 
-                                                        imagearray,
-                                                        self.uvgridconfig)
-             
-        mean_mse = evaluator.get_mean_mse()
-        
-        return mean_mse
-    
     def computeapproximatemse(self):
         """
         Evaluate approximate mean-square-error (MSE) on resulting image.
@@ -319,3 +457,87 @@ class AlmaSparseModeling(AlmaSparseModelingCore):
         acv = evaluator.evaluate(self.griddedvis)
         return 0.0
 
+
+class CVPlotter(object):
+    def __init__(self, nv, nh, L1_list, Ltsv_list):
+        self.nh = nh
+        self.nv = nv
+
+        self.left_margin = 0.1
+        self.right_margin = 0.1
+        self.bottom_margin = 0.1
+        self.top_margin = 0.1
+        total_width = 1.0 - (self.left_margin + self.right_margin)
+        total_height = 1.0 - (self.bottom_margin + self.top_margin)
+        dx = total_width / float(self.nh)
+        dy = total_height / float(self.nv)
+        self.dx = min(dx, dy)
+        self.dy = dx
+        f = pl.figure(num='CVPlot', figsize=(8,8))
+        pl.clf()
+        left = self.left_margin
+        bottom = self.bottom_margin
+        height = self.dy * self.nv
+        width = self.dx * self.nh
+        outer_frame = pl.axes([left, bottom, width, height])
+        outer_frame.set_xlim(-0.5, self.nh - 0.5)
+        outer_frame.set_ylim(-0.5, self.nv - 0.5)
+        outer_frame.set_xlabel('log10(Ltsv)')
+        outer_frame.set_ylabel('log10(L1)')
+        outer_frame.xaxis.set_major_locator(matplotlib.ticker.FixedLocator(range(self.nh)))
+        outer_frame.yaxis.set_major_locator(matplotlib.ticker.FixedLocator(range(self.nv)))
+        outer_frame.xaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, pos: int(math.log10(Ltsv_list[int(x)]))))
+        outer_frame.yaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, pos: int(math.log10(L1_list[int(x)]))))
+        
+        self.L1_list = L1_list
+        self.Ltsv_list = Ltsv_list
+        
+        self.axes_list = collections.defaultdict(dict)
+        
+    def plotimage(self, row, column, data, mse):
+        left = self.left_margin + column * self.dx
+        bottom = self.bottom_margin + row * self.dy
+        height = self.dx
+        width = self.dy
+        #print 'pl.axes([{0}, {1}, {2}, {3}])'.format(left, bottom, width, height)
+        nx, ny = data.shape
+        a = pl.axes([left, bottom, width, height])
+        a.imshow(numpy.flipud(data.transpose()))
+        a.text(nx-2, 5, '{:.5g}'.format(mse), ha='right', va='top', fontdict={'size': 'small', 'color': 'white'})
+        a.xaxis.set_major_locator(matplotlib.ticker.NullLocator())
+        a.yaxis.set_major_locator(matplotlib.ticker.NullLocator())
+        self.axes_list[row][column] = a
+
+    def mark_bestimage(self, row, column):
+        best_axes = self.axes_list[row][column]
+        bbox = best_axes.get_position()
+        best_frame = pl.axes(bbox, axisbg='none')
+        best_frame.xaxis.set_major_locator(matplotlib.ticker.NullLocator())
+        best_frame.yaxis.set_major_locator(matplotlib.ticker.NullLocator())
+        for loc, spine in best_frame.spines.iteritems():
+            spine.set_color('red')
+            spine.set_linewidth(3)
+            
+    def draw(self):
+        pl.draw()
+        
+    def savefig(self, figfile):
+        pl.savefig(figfile)
+
+        
+class NullPlotter(object):
+    def __init__(self, *args, **kwargs):
+        pass
+    
+    def plotimage(self, *args, **kwargs):
+        pass
+        
+    def mark_bestimage(self, *args, **kwargs):
+        pass
+
+    def draw(self, *args, **kwargs):
+        pass
+        
+    def savefig(self, *args, **kwargs):
+        pass
+    
