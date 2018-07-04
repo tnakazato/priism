192a0e7f7837ef56c56c022c872a41c496af4b1c
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2018-07-04 16:40:05 +0900

    Use atexit module to clean up libsakurapy
    Refs #1249

--- rism/external/sakura/__init__.py
+++ rism/external/sakura/__init__.py
@@ -1,11 +1,10 @@
 from __future__ import absolute_import
 
 import numpy
+import atexit
 
 from .reductionhelper import paraMap
 import libsakurapy
-
-# from ..casa import casa_atexit
 
 # libsakurapy is initialized when the module is imported
 # and is cleaned up at exit
@@ -17,10 +16,10 @@ except NameError:
     print 'LOG: initialize sakura...'
     print libsakurapy
     libsakurapy.initialize()
-#     def sakura_cleanup():
-#         print 'LOG: clean up sakura...'
-#         libsakurapy.clean_up()
-#     casa_atexit.register(sakura_cleanup)
+    def sakura_cleanup():
+        print 'LOG: clean up sakura...'
+        libsakurapy.clean_up()
+    atexit.register(sakura_cleanup)
 
 from .allocator import empty_aligned
 from .allocator import empty_like_aligned
