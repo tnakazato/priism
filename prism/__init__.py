e86771f5c51afc8a81fabb2b82279c3e0f7fd811
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2018-07-05 12:47:14 +0900

    delete __init__.py at wrong location.

--- prism/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-from .core import GriddedVisibilityStorage
-from .core import ResultingImageStorage
-from .core import UVGridConfig
-
-from .core import SparseModelingImager
