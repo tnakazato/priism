2e8ae161ecf2f9a295d4b33f3a1fc749cf0c68ab
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2018-07-10 11:44:37 +0900

    Impelement a code to get CASA version
    Refs #1253
    CASA version is taken from global scope. If appropriate variable is not
    available in the scope, the version will be set to -1.0.0-0.

--- prism/python/prism/external/casa/__init__.py
+++ prism/python/prism/external/casa/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+import collections
 
 # casa tool generators
 from .casatools import CasaToolGenerator
@@ -17,3 +18,38 @@ from .casatools import OpenMS
 from .casatools import OpenMSMetaData
 from .casatools import OpenImage
 
+# casa version
+def _get_casa_version():
+    try:
+        import inspect
+        version_string = 'NONE'
+        frames = inspect.getouterframes(inspect.currentframe())
+        for f in frames:
+            if 'casa' in f[0].f_globals:
+                casadict = f[0].f_globals['casa']
+                if 'version' in casadict:
+                    version_string = casadict['version']
+                    break
+    except:
+        version_string = 'NONE'
+    #print(version_string)
+    
+    # version string format: MAJOR.MINOR.PATCH-BUILD
+    s = version_string.split('-')
+    if len(s) != 2:
+        build = 0
+    else:
+        build = int(s[1])
+    t = s[0].split('.')
+    if len(t) != 3:
+        major = -1
+        minor = 0
+        patch = 0
+    else:
+        major = int(t[0])
+        minor = int(t[1])
+        patch = int(t[2])
+    CASAVersion = collections.namedtuple('CASAVersion', ['major', 'minor', 'patch', 'build'])
+    return CASAVersion(major=major, minor=minor, patch=patch, build=build)
+
+casa_version = _get_casa_version()
