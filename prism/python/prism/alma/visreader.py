d4913b7158495877aacead237ba9d263663f084e
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2018-07-10 11:45:59 +0900

    Switch method name depending on the CASA version
    Refs #1253

--- prism/python/prism/alma/visreader.py
+++ prism/python/prism/alma/visreader.py
@@ -1,12 +1,37 @@
 from __future__ import absolute_import
 
-import time
-
 from . import paramcontainer
 from . import visconverter
 import prism.external.casa as casa
 
 class VisibilityReader(object):
+    def __method_name(self, basename):
+        if casa.casa_version.major == 5:
+            if casa.casa_version.minor >= 3:
+                return basename
+            elif casa.casa_version.minor >= 1:
+                return '{}2'.format(basename)
+        raise RuntimeError('Unsupported CASA version {}.{}.{}-{}'.format(casa.casa_version.major,
+                                                                         casa.casa_version.minor,
+                                                                         casa.casa_version.major,
+                                                                         casa.casa_version.build))       
+    
+    @property
+    def iterinit(self):
+        return self.__method_name('iterinit')
+    
+    @property
+    def iterorigin(self):
+        return self.__method_name('iterorigin')
+    
+    @property
+    def getdata(self):
+        return self.__method_name('getdata')
+    
+    @property
+    def iternext(self):
+        return self.__method_name('iternext') 
+    
     """
     VisibilityReader is responsible for scanning and reading MS 
     according to user-specified data selection (MS name, 
@@ -43,17 +68,23 @@ class VisibilityReader(object):
 
         with casa.OpenMS(vis) as ms:
             ms.msselect(msselect, onlyparse=False)
+            
+            # method names depend on CASA version
+            ms_iterinit = getattr(ms, self.iterinit)
+            ms_iterorigin = getattr(ms, self.iterorigin)
+            ms_getdata = getattr(ms, self.getdata)
+            ms_iternext = getattr(ms, self.iternext)
         
             # not using new iterator as it doesn't support 
             # reading meta information such as UVW...
             # iterate through MS using VI/VB2 framework
-            ms.iterinit2(columns, interval, nrow, adddefault)
-            more_chunks = ms.iterorigin2()
+            ms_iterinit(columns, interval, nrow, adddefault)
+            more_chunks = ms_iterorigin()
             chunk_id = 0
             while (more_chunks):
-                rec = ms.getdata2(items)
+                rec = ms_getdata(items)
                 rec['chunk_id'] = chunk_id
                 print 'LOG: read visibility chunk #{0}'.format(chunk_id)
                 yield rec
-                more_chunks = ms.iternext2()
+                more_chunks = ms_iternext()
                 chunk_id += 1
