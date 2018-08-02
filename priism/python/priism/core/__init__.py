9a306ef1700c02d6b2ef5c2b68085196807abe6d
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2018-08-02 11:38:59 +0900

    Removed unnecessary comment

--- priism/python/priism/core/__init__.py
+++ priism/python/priism/core/__init__.py
@@ -1,7 +1,5 @@
 from __future__ import absolute_import
 
-print('This is core')
-
 from .datacontainer import GriddedVisibilityStorage
 from .datacontainer import ResultingImageStorage
 from .datacontainer import UVGridConfig
