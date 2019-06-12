9792207fb82d1f3504bfb0876dec083233ca0152
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2019-06-12 14:01:59 +0900

    Minor formatting

--- python/priism/test/cvrun.py
+++ python/priism/test/cvrun.py
@@ -36,7 +36,7 @@ L1_list = [1e0, 1e2, 1e4, 1e6, 1e7, 1e8]
 Ltsv_list = [1e0, 1e2, 1e4, 1e6, 1e8, 1e10]
 
 # set num_fold to 0 to disable CV
-num_fold=10
+num_fold = 10
 worker.cvforgridvis(L1_list, Ltsv_list, num_fold=num_fold,
                     imageprefix='hd142527', imagepolicy='full',
                     summarize=True, figfile='cvresult.png',
