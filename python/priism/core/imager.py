69f441991b8ce96e855a76dc5f398c371ffb29d0
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2019-06-12 11:46:41 +0900

    Formatting

--- python/priism/core/imager.py
+++ python/priism/core/imager.py
@@ -19,19 +19,20 @@ from . import paramcontainer
 from . import mfista
 from . import cv
 
+
 class SparseModelingImager(object):
     """
     Core implementation of sparse modeling specialized for ALMA.
     It performs visibility gridding on uv-plane.
     """
-    
+
     @property
     def griddedvis(self):
         """
         TODO
         """
         return getattr(self, '_griddedvis', None)
-    
+
     @griddedvis.setter
     def griddedvis(self, value):
         if value is None:
@@ -40,13 +41,13 @@ class SparseModelingImager(object):
             self._griddedvis = value
         else:
             raise TypeError('Given value is not an instance of GriddedVisibilityStorage')
-        
+
     @property
     def working_set(self):
         """
         """
         return getattr(self, '_working_set', None)
-    
+
     @working_set.setter
     def working_set(self, value):
         if value is None:
@@ -55,14 +56,14 @@ class SparseModelingImager(object):
             self._working_set = value
         else:
             raise TypeError('Given value is an instance of VisibilityWorkingSet')
-        
+
     @property
     def imagearray(self):
         """
         TODO
         """
         return getattr(self, '_imagearray', None)
-    
+
     @imagearray.setter
     def imagearray(self, value):
         if value is None:
@@ -71,14 +72,14 @@ class SparseModelingImager(object):
             self._imagearray = value
         else:
             raise TypeError('Given value is not an instance of ResultingImageStorage')
-        
+
     @property
     def uvgridconfig(self):
         """
         TODO
         """
         return getattr(self, '_uvgridconfig', None)
-    
+
     @uvgridconfig.setter
     def uvgridconfig(self, value):
         if value is None:
@@ -87,7 +88,7 @@ class SparseModelingImager(object):
             self._uvgridconfig = value
         else:
             raise TypeError('Given value is not an instance of UVGridConfig')
-    
+
     @property
     def imagesuffix(self):
         """
@@ -96,11 +97,11 @@ class SparseModelingImager(object):
         exportimage method.
         """
         return 'pickle'
-    
+
     def __init__(self, solver='mfista_fft'):
         """
         Constructor
-        
+
         Parameters:
             solver  name of the solver
                     choices are as follows.
@@ -110,30 +111,30 @@ class SparseModelingImager(object):
        """
         self.solver_name = solver
         self.__initialize()
-    
+
     def __initialize(self):
         # configuration
         self.imparam = paramcontainer.SimpleImageParamContainer(imsize=100)
         self.visparams = []
-        
+
         # working array
         self.griddedvis = None
         self.imagearray = None
-        
+
         # TODO: optimize number of threads
         self.num_threads = 2
-        
+
         # create MFISTA instance with dummy parameter
         mfistaparam = paramcontainer.MfistaParamContainer(l1=0.0, ltsv=0.0)
         solver_cls = mfista.SolverFactory(self.solver_name)
         self.solver = solver_cls(mfistaparam, self.imparam)
-        
+
     def mfista(self, l1, ltsv, maxiter=50000, eps=1.0e-5, clean_box=None, 
                storeinitialimage=True, overwriteinitialimage=False):
         """
         Run MFISTA algorithm on gridded visibility data.
         gridvis must be executed beforehand.
-        
+
         Parameters:
             l1 -- L1 regularization term
             ltsv -- TSV regularization term
@@ -149,19 +150,19 @@ class SparseModelingImager(object):
         arr = self._mfista(self.mfistaparam, self.working_set,
                            storeinitialimage=storeinitialimage, overwriteinitialimage=overwriteinitialimage)
         self.imagearray = datacontainer.ResultingImageStorage(arr)
-    
+
     def _mfista(self, mfistaparam, working_set, storeinitialimage=True, overwriteinitialimage=False):
         assert working_set is not None
         self.solver.mfistaparam = mfistaparam
         return self.solver.solve(working_set, self.imparam, storeinitialimage, overwriteinitialimage)
-    
+
     def importvis(self, data=None, weight=None, filename=None, flipped=False, uvgrid=None):
         """
         Import visibility data. Users can provide visibility data either as numpy array 
         (data and weight) or as filename that stores visibility data in a specified format.
         Either data or filename should be specified. If both are specified, filename takes 
         priority.
-        
+
         Parameters:
             data     -- Visibility data as numpy complex array. Its shape must effectively 
                         be two-dimensional (nv, nu). For unflipped array, total power component
@@ -177,16 +178,12 @@ class SparseModelingImager(object):
                         In this case, equal weight (1.0) will be applied to all visibilities.
             filename -- Name of the file that stores visibility data and weights. Format 
                         should be as follows:
-                        
-                        TODO
-                        
-                        
             flipped  -- Whether or not given data and weight are flipped for FFT.
             uvgrid   -- (Optional) Configuration of uv grid as a UVGridConfig instance.
         """
         if data is None and filename is None:
             raise RuntimeError('data or filename must be specified')
-        
+
         if filename is not None:
             # filename is specified. read it.
             griddedvis = datacontainer.GriddedVisibilityStorage.importdata(filename)
@@ -203,11 +200,11 @@ class SparseModelingImager(object):
             if weight is None:
                 # use default weight (all 1.0)
                 weight = numpy.ones(datashape, dtype=numpy.float32)
-                
+
             weightshape = weight.shape
             if datashape != weightshape:
                 raise RuntimeError('Array shape of weight must conform with that of data.')
-            
+
             if len(datashape) == 2:
                 newdata = numpy.expand_dims(numpy.expand_dims(data, axis=-1), axis=-1)
                 newweight = numpy.reshape(weight, newdata.shape)
@@ -223,23 +220,23 @@ class SparseModelingImager(object):
                 newweight = weight
             else:
                 raise RuntimeError('Invalid array shape {}'.format(list(datashape)))
-            
+
             if newdata.dtype not in (numpy.complex, complex):
                 raise TypeError('data must be float complex array')
-            
+
             realdata = newdata.real
             imagdata = newdata.imag
-            
+
             if newweight.dtype not in (numpy.float32, numpy.float, float, numpy.complex, complex):
                 raise TypeError('weight must be float or float complex array')
-                
+
             if newweight.dtype in (numpy.float32, numpy.float, float):
                 realweight = newweight
                 imagweight = None
             else:
                 realweight = newweight.real
                 imagweight = newweight.imag
-            
+
         # flip back operation if necessary
         if flipped == True:
             realdata = numpy.fft.fftshift(realdata)
@@ -247,12 +244,12 @@ class SparseModelingImager(object):
             realweight = numpy.fft.fftshift(realweight)
             if imagweight is not None:
                 imagweight = numpy.fft.fftshift(imagweight)
-            
+
         self.griddedvis = datacontainer.GriddedVisibilityStorage(grid_real=realdata,
                                                                  grid_imag=imagdata,
                                                                  wgrid_real=realweight,
                                                                  wgrid_imag=imagweight)
-        
+
         if uvgrid is not None:
             self.uvgridconfig = uvgrid
         else:
@@ -262,46 +259,46 @@ class SparseModelingImager(object):
             cell = 1.0
             self.uvgridconfig = datacontainer.UVGridConfig(cellu=cell, cellv=cell, 
                                                            nu=nu, nv=nv)
-    
+
     def exportimage(self, imagename, overwrite=False):
         """
         Export MFISTA result as a cPickle object.
         mfista must be executed beforehand.
-         
+
         Parameters:
             imagename  name of output image name
         """
         if self.imagearray is None:
             raise RuntimeError('You don\'t have an image array!')
-        
+
         if os.path.exists(imagename) and overwrite == False:
             raise RuntimeError('Cannot overwrite existing file "{}"'.format(imagename))
-        
+
         with open(imagename, 'wb') as f:
             pickle.dump(self.imagearray, f)
 
     def getimage(self, imagename):
         """
         Get image data from exported file
-        
+
         Parameters:
             imagename  name of image to be read
         """
         if not os.path.exists(imagename):
             raise RuntimeError('image "{}" does not exist'.format(imagename))
-        
+
         with open(imagename, 'rb') as f:
             data = pickle.load(f)
-            
+
         return data
 
     def cvforgridvis(self, l1_list, ltsv_list, num_fold=10, imageprefix='image', imagepolicy='full', 
-               summarize=True, figfile=None, datafile=None, maxiter=50000, eps=1.0e-5, clean_box=None,
-               resultasinitialimage=True):
+                     summarize=True, figfile=None, datafile=None, maxiter=50000, eps=1.0e-5, clean_box=None,
+                     resultasinitialimage=True):
         """
         Perform cross validation and search the best parameter for L1 and Ltsv from 
         the given list of these.
-        
+
         Inputs:
             l1_list -- list of L1 values to examine
             ltsv_list -- List of Ltsv values to examine
@@ -320,17 +317,17 @@ class SparseModelingImager(object):
             eps -- threshold factor for MFISTA algorithm
             clean_box -- clean box as a float array (default None)
             resultasinitialimage -- keep resulting image as an initial condition for next run
-        
+
         Output:
             dictionary containing best L1 (key: L1), best Ltsv (key;Ltsv), and
             corresponding image name (key: image, should be <imageprefix>.fits)
         """
         start_time = time.time()
-        
+
         # sanity check
         if imagepolicy not in ('best', 'full'):
             raise ArgumentError('imagepolicy must be best or full. {0} was provided.'.format(imagepolicy))
-        
+
         try:
             np_l1_list = numpy.asarray(l1_list)
             np_ltsv_list = numpy.asarray(ltsv_list)
@@ -338,54 +335,52 @@ class SparseModelingImager(object):
             print('Exception occurred')
             print(str(e))
             raise ArgumentError('l1_list or ltsv_list (or both) seems invalid.')
-        
+
         if str(np_l1_list.dtype) == 'object':
             raise ArgumentError('l1_list contains invalid value')
         if str(np_ltsv_list.dtype) == 'object':
             raise ArgumentError('ltsv_list contains invalid value')
-        
+
         result_L1 = []
         result_Ltsv = []
         result_mse = []
         result_image = []
-        
+
         num_L1 = len(np_l1_list)
         num_Ltsv = len(np_ltsv_list)
         L1_sort_index = numpy.argsort(np_l1_list)
         Ltsv_sort_index = numpy.argsort(np_ltsv_list)
-        
+
         sorted_l1_list = np_l1_list[L1_sort_index]
         sorted_ltsv_list = np_ltsv_list[Ltsv_sort_index]
-        
+
         if summarize is True:
             PlotterClass = CVPlotter
         else:
             PlotterClass = NullPlotter
         plotter = PlotterClass(num_L1, num_Ltsv, sorted_l1_list, sorted_ltsv_list)
-            
+
         if datafile is not None:
             f = open(datafile, 'w')
         else:
             f = open(os.devnull, 'w')
         print('# L1, Ltsv, MSE', file=f)
-        
+
         # initialize CV
         self.initializecv(num_fold=num_fold)
-                
-        #for j, Ltsv in enumerate(np_ltsv_list):
+
         # loop Ltsv in ascending order
         for j in range(num_Ltsv):
             Ltsv = sorted_ltsv_list[j]
             # trick to update initial image when Ltsv is changed
             overwrite_initial = True
-            
-            #for i, L1 in enumerate(np_l1_list):
+
             # loop L1 in descending order
             for i in range(num_L1 - 1, -1, -1):
                 L1 = sorted_l1_list[i]
                 result_L1.append(L1)
                 result_Ltsv.append(Ltsv)
-                
+
                 # get full visibility image first
                 imagename = '{}_L1_{}_Ltsv_{}.{}'.format(imageprefix, 
                                                          int(math.log10(L1)), int(math.log10(Ltsv)),
@@ -395,49 +390,49 @@ class SparseModelingImager(object):
                             overwriteinitialimage=overwrite_initial)
                 self.exportimage(imagename, overwrite=True)
                 result_image.append(imagename)
-                
+
                 # then evaluate MSE
                 mse = self.computemse(L1, Ltsv, maxiter, eps, clean_box)
                 result_mse.append(mse)
-                
+
                 print('L1 10^{0} Ltsv 10^{1}: MSE {2} FITS {3}'.format(int(math.log10(L1)),
                                                                        int(math.log10(Ltsv)),
                                                                        mse,
                                                                        imagename))
                 print('{0}, {1}, {2}'.format(L1, Ltsv, mse), file=f)
-        
+
                 if summarize is True:
                     imagearray = self.getimage(imagename)
                     data = numpy.squeeze(imagearray.data) #data will be 2D
                     plotter.plotimage(i, j, data, mse)
-                    
+
                 # As long as Ltsv is kept, initial image will not be updated
                 #overwrite_initial = False
-                            
+
         # finalize CV
         self.finalizecv()
-        
+
         f.close()
-        
+
         best_solution = numpy.argmin(result_mse)
         best_mse = result_mse[best_solution]
         best_image = result_image[best_solution]
         best_L1 = result_L1[best_solution]
         best_Ltsv = result_Ltsv[best_solution]
-        
+
 #         L1_index = np_l1_list.tolist().index(best_L1)
 #         Ltsv_index = np_ltsv_list.tolist().index(best_Ltsv)
         L1_index = numpy.where(sorted_l1_list == best_L1)[0][0]
         Ltsv_index = numpy.where(sorted_ltsv_list == best_Ltsv)[0][0]
         if best_mse >= 0.0:
             plotter.mark_bestimage(L1_index, Ltsv_index)
-        
+
         plotter.draw()
         if figfile is not None:
             plotter.savefig(figfile)
         # completed
         end_time = time.time()
-        
+
         if best_mse >= 0.0:
             print('Process completed. Optimal result is as follows')
             print('    L1, Ltsv = 10^{0}, 10^{1}'.format(int(math.log10(best_L1)), int(math.log10(best_Ltsv))))
@@ -446,10 +441,9 @@ class SparseModelingImager(object):
         else:
             print('Process completed. Cross-validation was not performed.')
             print('WARNING: Optimal solution will not be correct one since no CV was executed.')
-        
+
         print('Elapsed {0} sec'.format(end_time - start_time))
-        
-        
+
         # copy the best image to final image
         shutil.copy2(best_image, imageprefix+'.'+self.imagesuffix)
         if imagepolicy == 'full':
@@ -461,20 +455,19 @@ class SparseModelingImager(object):
                 os.remove(imagename)
         else:
             assert False
-            
+
         # finally, return best L1 and Ltsv
         return {'L1': best_L1, 'Ltsv': best_Ltsv}
-           
 
     def initializecv(self, num_fold=10):
         assert self.working_set is not None
-        
+
         if (not hasattr(self, 'visset')) or self.visset is None:
             self.visset = cv.VisibilitySubsetGenerator(self.working_set, num_fold) 
-    
+
     def finalizecv(self):
         self.visset = None
-    
+
     def computemse(self, l1, ltsv, maxiter=50000, eps=1.0e-5, clean_box=None):
         """
         Compute mean-square-error (MSE) on resulting image.
