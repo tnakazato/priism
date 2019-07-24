7c53830b1b0c5c72bf1f3ae03ad0147009b6e08d
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2019-07-24 13:58:55 +0900

    Add copyright notice
    Refs #1346

--- python/priism/external/sakura/reductionhelper.py
+++ python/priism/external/sakura/reductionhelper.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2019
+# National Astronomical Observatory of Japan
+# 2-21-1, Osawa, Mitaka, Tokyo, 181-8588, Japan.
+#
+# This file is part of PRIISM.
+#
+# PRIISM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# PRIISM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with PRIISM.  If not, see <https://www.gnu.org/licenses/>.
 import contextlib
 import itertools
 import multiprocessing
