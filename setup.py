90f2c333ed78685080f8c2cdd5d61743498f29b3
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2020-01-09 17:11:09 +0900

    test for setuptools

---
+++ setup.py
@@ -0,0 +1,9 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+#    name='priism',
+#    version='0.0.0',
+#    packages=find_packages('python', exclude=['priism.test']),
+#    package_dir={'':'python'}
+)
