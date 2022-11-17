61497c3ab18827a3e4b8dfc0dd244e4299d2c460
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2022-11-17 16:46:51 +0900

    update libsakura version to 5.1.5

--- setup.py
+++ setup.py
@@ -295,7 +295,7 @@ class download_sakura(config):
 
         package = 'sakura'
         target = 'libsakura'
-        version = 'libsakura-5.1.4'
+        version = 'libsakura-5.1.5'
         zipname = f'{version}.zip'
         base_url = 'https://github.com/tnakazato/sakura'
         if IS_GIT_OK:
