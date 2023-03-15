c4a3e7181f71959a02ff67545ea4d1461809d29c
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2023-03-15 15:45:16 +0900

    Revert libsakura version to 5.1.6

--- setup.py
+++ setup.py
@@ -298,7 +298,7 @@ class download_sakura(config):
 
         package = 'sakura'
         target = 'libsakura'
-        version = 'libsakura-5.1.5'
+        version = 'libsakura-5.1.6'
         zipname = f'{version}.zip'
         base_url = 'https://github.com/tnakazato/sakura'
         if IS_GIT_OK:
